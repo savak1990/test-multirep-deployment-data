feeebced8920077cc24bc1b036ec2ae286d3ac63
savak1990 <slava.klovan@gmail.com>
2024-09-18 14:28:44 +0200

    Fix instance_type to run for mysql

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 resource "aws_db_instance" "example" {
-  identifier_prefix = var.db_identifier_prefix
-  engine = "mysql"
-  allocated_storage = 10
-  instance_class = "db.t2.micro"
+  identifier_prefix   = var.db_identifier_prefix
+  engine              = "mysql"
+  allocated_storage   = 10
+  instance_class      = "db.t4g.micro"
   skip_final_snapshot = true
-  
-  db_name = var.db_name
+
+  db_name  = var.db_name
   username = var.db_username
   password = var.db_password
 }
