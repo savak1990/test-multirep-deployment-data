717c6862928d536b7cf9d4bc1fbc7ff863bcf107
savak1990 <slava.klovan@gmail.com>
2024-09-20 14:05:54 +0200

    Update allocated storage config for replica

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 resource "aws_db_instance" "example" {
   identifier_prefix   = var.db_identifier_prefix
-  allocated_storage   = 10
   instance_class      = "db.t4g.micro"
   skip_final_snapshot = true
 
@@ -8,10 +7,11 @@ resource "aws_db_instance" "example" {
 
   replicate_source_db = var.replicate_source_db
 
-  engine   = var.replicate_source_db == null ? "mysql" : null
-  db_name  = var.replicate_source_db == null ? var.db_name : null
-  username = var.replicate_source_db == null ? var.db_username : null
-  password = var.replicate_source_db == null ? var.db_password : null
+  allocated_storage = var.replicate_source_db == null ? 10 : null
+  engine            = var.replicate_source_db == null ? "mysql" : null
+  db_name           = var.replicate_source_db == null ? var.db_name : null
+  username          = var.replicate_source_db == null ? var.db_username : null
+  password          = var.replicate_source_db == null ? var.db_password : null
 }
 
 terraform {
