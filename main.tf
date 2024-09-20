5465b68998f948d0aac4e363d844634233448c96
savak1990 <slava.klovan@gmail.com>
2024-09-20 13:52:15 +0200

    Add replica support to the module

--- main.tf
+++ main.tf
@@ -1,11 +1,24 @@
 resource "aws_db_instance" "example" {
   identifier_prefix   = var.db_identifier_prefix
-  engine              = "mysql"
   allocated_storage   = 10
   instance_class      = "db.t4g.micro"
   skip_final_snapshot = true
 
-  db_name  = var.db_name
-  username = var.db_username
-  password = var.db_password
+  backup_retention_period = var.backup_retention_period
+
+  replicate_source_db = var.replicate_source_db
+
+  engine   = var.replicate_source_db == null ? "mysql" : null
+  db_name  = var.replicate_source_db == null ? var.db_name : null
+  username = var.replicate_source_db == null ? var.db_username : null
+  password = var.replicate_source_db == null ? var.db_password : null
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,21 +1,36 @@
 variable "db_username" {
   description = "The username for the database"
-  type = string
-  sensitive = true
+  type        = string
+  sensitive   = true
+  default     = null
 }
 
 variable "db_password" {
   description = "The password for the database"
-  type = string
-  sensitive = true
+  type        = string
+  sensitive   = true
+  default     = null
 }
 
 variable "db_identifier_prefix" {
   description = "Identfier prefix of Database"
-  type = string
+  type        = string
 }
 
 variable "db_name" {
   description = "Name of the database"
-  type = string
+  type        = string
+  default     = null
+}
+
+variable "backup_retention_period" {
+  description = "Days to retain backups. Must be > 0 to enable replication"
+  type        = number
+  default     = null
+}
+
+variable "replicate_source_db" {
+  description = "If specified, replicate the RDS database at given ARN"
+  type        = string
+  default     = null
 }
